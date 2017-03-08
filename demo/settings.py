9510647170bad1319c0e70b1db2a18f6f0ee268e
yurs <yurs@gmail.com>
2017-03-08 07:11:53 -0800

    copy paste de amb -> new project test pero no cambio nada

--- demo/settings.py
+++ demo/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = (
     'viewflow.frontend',
     'demo.helloworld',
     'demo.amb',
+    'demo.test',
 )
 
 MIDDLEWARE_CLASSES = (
---
+++ demo/test/flows.py
@@ -0,0 +1,48 @@
+from viewflow import flow
+from viewflow.base import this, Flow
+from viewflow.flow.views import CreateProcessView, UpdateProcessView
+from viewflow import frontend
+
+from .models import testProcess
+
+
+@frontend.register
+class testFlow(Flow):
+    process_class = testProcess
+
+    start = (
+        flow.Start(
+            CreateProcessView,
+            #fields=["text"]
+            fields=["text", "email"]
+            #fields=["email"]
+        ).Permission(
+            auto_create=True
+        ).Next(this.approve)
+    )
+
+    approve = (
+        flow.View(
+            UpdateProcessView,
+            fields=["approved"]
+        ).Permission(
+            auto_create=True
+        ).Next(this.check_approve)
+    )
+
+    check_approve = (
+        flow.If(lambda activation: activation.process.approved)
+        .Then(this.send)
+        .Else(this.end)
+    )
+
+    send = (
+        flow.Handler(
+            this.send_hello_world_request1
+        ).Next(this.end)
+    )
+
+    end = flow.End()
+
+    def send_hello_world_request1(self, activation):
+        print(activation.process.text)
